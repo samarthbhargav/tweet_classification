c57f81e37ca5ebd52a8327c2ee7982960984ca3c
samarthbhargav <samarth.bhargav92@gmail.com>
2014-06-28 02:09:07 +0530

    added skeleton code

--- classify.py
+++ classify.py
@@ -1,1 +1,20 @@
+import twitter_api
+import config
+
+
+class TweetClassifier(object):
+
+    def __init__(self):
+        pass
+
+    def train(self):
+        pass
+
+    def classify(self):
+        pass
+
+
+
+if __name__ == "__main__":
+    clf = TweetClassifier()
 
--- twitter_api.py
+++ twitter_api.py
@@ -19,19 +19,38 @@ twitter_conf = TwitterConfig()
 class TweetListener(StreamListener):
     # A listener handles tweets are the received from the stream.
     #This is a basic listener that just prints received tweets to standard output
- 
+
     def on_data(self, data):
         js = json.loads(data, encoding="utf-8")
         print js['text'].encode('ascii', 'ignore')
         return True
- 
+
     def on_error(self, status):
         print status
- 
-#printing all the tweets to the standard output
-auth = OAuthHandler(twitter_conf.consumer_key, twitter_conf.consumer_secret)
-auth.set_access_token(twitter_conf.access_token, twitter_conf.access_secret)
- 
-stream = Stream(auth, TweetListener())
-print stream.filter(track=['india'])
-#api = tweepy.API(auth)
+
+
+class TweetStream(object):
+
+    def __init__(self, stream):
+        self.__stream = stream
+
+
+    def kill(self):
+        pass
+
+    def get_tweets(self):
+        pass
+
+
+class ShortTweetStream(TweetStream):
+
+    def get_stream_as_list(self):
+        pass
+
+if __name__ == "__main__":
+    auth = OAuthHandler(twitter_conf.consumer_key, twitter_conf.consumer_secret)
+    auth.set_access_token(twitter_conf.access_token, twitter_conf.access_secret)
+
+    stream = Stream(auth, TweetListener())
+    print stream.filter(track=['india'])
+    #api = tweepy.API(auth)
