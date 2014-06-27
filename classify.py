eeb632aff32da82f4e274fe13b17d8ef8fa5eb2e
samarthbhargav <samarth.bhargav92@gmail.com>
2014-06-28 02:49:21 +0530

    Added Classifier code

--- classify.py
+++ classify.py
@@ -1,19 +1,41 @@
 import twitter_api
-import config
+
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 
 class TweetClassifier(object):
 
     def __init__(self):
-        pass
+        self._classifier = MultinomialNB()
+        self._corpus = None
+        self._vectorizer = TfidfVectorizer(sublinear_tf=True, max_df=0.5, stop_words='english', analyzer="word")
 
-    def train(self):
-        pass
+    def set_corpus(self, corpus):
+        """
+        Sets the classifiers corpus
+            Must be a list of iterables
+        """
+        self._corpus = []
+        for element in corpus:
+            self._corpus.append(element)
 
-    def classify(self):
-        pass
+    def append_to_corpus(self, to_append):
+        if self._corpus is None:
+            self._corpus = []
+        self._corpus.append(to_append)
 
 
+    def train(self, train_data, train_labels):
+        if self._corpus is None:
+            raise Exception("Corpus is not set")
+        self._vectorizer.fit(self._corpus)
+        train_vectors = self._vectorizer.transform(train_data)
+        self._classifier.fit(train_vectors, train_labels)
+
+    def predict(self, data):
+        vector = self._vectorizer.transform([data])
+        return self._classifier.predict(vector)
 
 if __name__ == "__main__":
     clf = TweetClassifier()
